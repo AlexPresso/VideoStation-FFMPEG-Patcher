782da398a3adf8d76aad5568dec1b61d428a539f
AlexPresso <blond.alexandre@hotmail.com>
2021-04-12 22:26:27 +0200

    Ident

--- patcher.sh
+++ patcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function save_current() {
-  echo "[INFO] Saving current ffmpeg as ffmpeg.orig"
+	echo "[INFO] Saving current ffmpeg as ffmpeg.orig"
 	mv -n /var/packages/VideoStation/target/lib/ffmpeg /var/packages/VideoStation/target/lib/ffmpeg.orig
 }
 
@@ -13,7 +13,7 @@ function save_and_patch() {
 }
 
 function armv8_procedure() {
-  save_current
+	save_current
 
 	echo "[INFO] Downloading patched ffmpeg files to /var/packages/VideoStation/target/lib"
 	echo ""
@@ -30,36 +30,36 @@ function armv8_procedure() {
 	)
 
 	if [[! -d "/var/packages/VideoStation/target/lib/ffmpeg"]]; then
-	  mkdir "/var/packages/VideoStation/target/lib/ffmpeg"
-  fi
+		mkdir "/var/packages/VideoStation/target/lib/ffmpeg"
+	fi
 
 	for file in "${ffmpegfiles[@]}"
 	do
-	  echo "[INFO] Downloading $file..."
+		echo "[INFO] Downloading $file..."
 		wget -q -O "/var/packages/VideoStation/target/lib/ffmpeg/$file" "https://github.com/AlexPresso/VideoStation-FFMPEG-Patcher/blob/main/ffmpeg/$file?raw=true"
 	done
 
-  save_and_patch
+  	save_and_patch
 
 	echo ""
 	echo "[SUCCESS] Done patching, please restart VideoStation (stop and start from package center)"
 }
 
 function others_procedure() {
-  save_current
+  	save_current
 
-  wget -O - https://gist.githubusercontent.com/BenjaminPoncet/bbef9edc1d0800528813e75c1669e57e/raw/ffmpeg-wrapper > /var/packages/VideoStation/target/bin/ffmpeg
+  	wget -O - https://gist.githubusercontent.com/BenjaminPoncet/bbef9edc1d0800528813e75c1669e57e/raw/ffmpeg-wrapper > /var/packages/VideoStation/target/bin/ffmpeg
 
-  chown root:VideoStation /var/packages/VideoStation/target/bin/ffmpeg
-  chmod 750 /var/packages/VideoStation/target/bin/ffmpeg
-  chmod u+s /var/packages/VideoStation/target/bin/ffmpeg
+  	chown root:VideoStation /var/packages/VideoStation/target/bin/ffmpeg
+  	chmod 750 /var/packages/VideoStation/target/bin/ffmpeg
+  	chmod u+s /var/packages/VideoStation/target/bin/ffmpeg
 
-  save_and_patch
+  	save_and_patch
 }
 
 if [[ $(cat /proc/cpuinfo | grep 'model name' | uniq) =~ "ARMv8" ]]; then
-  armv8_procedure
+  	armv8_procedure
 else
-  others_procedure
+  	others_procedure
 fi
 
