76401bdf3f06c7fe9fdf9798e82cf101e177da20
Alex <15526962+AlexPresso@users.noreply.github.com>
2023-11-15 20:53:53 +0100

    Fix config and logs (#111)
    * Fix config load and load error log

--- issue-report.sh
+++ issue-report.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 ###############################
-# VARS
+# VARS + EXTERNAL FILES
 ###############################
 
 ffmpeg_version=ffmpeg
+
 # shellcheck source=/.github/workflows/mock/VERSION
 source "/etc/VERSION"
+
 # shellcheck source=/utils/patch_config.sh
-source "/var/packages/VideoStation/patch_config.sh" || echo "No patch_config.sh"
+source "/var/packages/VideoStation/patch/patch_utils.sh" 2> /dev/null ||
+source "/var/packages/CodecPack/patch/patch_utils.sh" 2> /dev/null ||
+echo "No patch_config.sh"
+
+# shellcheck source=/utils/patch_config.sh
+source "/var/packages/VideoStation/patch/patch_config.sh" 2> /dev/null ||
+source "/var/packages/CodecPack/patch/patch_config.sh" 2> /dev/null ||
+echo "No patch_utils.sh"
+
 dsm_version="$productversion $buildnumber-$smallfixnumber"
 vs_path=/var/packages/VideoStation
 cp_path=/var/packages/CodecPack
