235e2853b04493a6f38f22192cc39b73685d2936
Alex <15526962+AlexPresso@users.noreply.github.com>
2023-12-22 13:27:34 +0100

    Remove external script sources (#117)

--- utils/patch_config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC2034
-ffmpeg_version=@ffmpeg_version@
--- wrappers/ffmpeg.sh
+++ wrappers/ffmpeg.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# shellcheck source=/utils/patch_utils.sh
-source "/var/packages/VideoStation/patch/patch_utils.sh" 2> /dev/null ||
-source "/var/packages/CodecPack/patch/patch_utils.sh" 2> /dev/null ||
-{ echo "Cannot load patch_utils.sh" >> "/tmp/ffmpeg-0.stderr.prev" && echo "Cannot load patch_utils.sh" && exit 1; }
-
 #########################
 # VARS
 #########################
 
-ffmpeg_version=ffmpeg
+ffmpeg_version=@ffmpeg_version@
 pid=$$
 child=""
 stderrfile="/tmp/ffmpeg-$pid.stderr"
@@ -18,13 +13,90 @@ path=$(realpath "$0")
 args=()
 
 #########################
-# LOAD CONFIG
+# UTILS
 #########################
 
-# shellcheck source=/utils/patch_config.sh
-source "/var/packages/VideoStation/patch/patch_config.sh" 2> /dev/null ||
-source "/var/packages/CodecPack/patch/patch_config.sh" 2> /dev/null ||
-{ echo "Cannot load patch_config.sh" >> "/tmp/ffmpeg-0.stderr.prev" && echo "Cannot load patch_config.sh" && exit 1; }
+log() {
+  local now
+  now=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "[$now] [$1] $2" >> "$stderrfile"
+}
+
+newline() {
+  echo "" >> "$stderrfile"
+}
+
+info() {
+  log "INFO" "$1"
+}
+
+kill_child() {
+  if [[ "$child" != "" ]]; then
+    kill "$child" > /dev/null 2> /dev/null || :
+  fi
+}
+
+endprocess() {
+  info "========================================[end $0 $pid]"
+  newline
+
+  if [[ $errcode -eq 1 ]]; then
+    cp "$stderrfile" "$stderrfile.prev"
+  fi
+
+  kill_child
+  rm -f "$stderrfile"
+
+  exit "$errcode"
+}
+
+handle_error() {
+  log "ERROR" "An error occurred"
+  newline
+  errcode=1
+  kill_child
+}
+
+fix_args() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -acodec)
+        shift
+        if [[ "$1" = "libfaac" ]]; then
+          args+=("-acodec" "aac")
+        else
+          args+=("-acodec" "libfdk_aac")
+        fi
+        ;;
+
+      -vf)
+        shift
+        arg="$1"
+
+        if [[ "$arg" =~ "scale_vaapi" ]]; then
+          scale_w=$(echo "$arg" | sed -n 's/.*w=\([0-9]\+\):h=\([0-9]\+\).*/\1/p')
+          scale_h=$(echo "$arg" | sed -n 's/.*w=\([0-9]\+\):h=\([0-9]\+\).*/\2/p')
+
+          if (( scale_w && scale_h )); then
+            arg="scale_vaapi=w=$scale_w:h=$scale_h:format=nv12,hwupload,setsar=sar=1"
+          else
+            arg="scale_vaapi=format=nv12,hwupload,setsar=sar=1"
+          fi
+        fi
+
+        args+=("-vf" "$arg")
+        ;;
+
+      -r)
+        shift
+        ;;
+
+      *) args+=("$1") ;;
+    esac
+
+    shift
+  done
+}
 
 #########################
 # ENTRYPOINT
